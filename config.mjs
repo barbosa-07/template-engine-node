e6b0533460975d0a2bb4545429d78b4e699538aa
Md Rizwan Akram <rizwanakram4049@gmail.com>
2021-10-07 02:07:09 +0530

    Adds portfolio templte

--- config.mjs
+++ config.mjs
@@ -1,14 +1,18 @@
 export const sampleTemplateData = {
-    fileName:"sample.html",
-    websiteTitle: "My template",
-    title: "This is a generaated title",
-    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem corrupti debitis libero vitae saepe totam. Praesentium dolorem amet nihil placeat."
-}
+  fileName: "sample.html",
+  websiteTitle: "My template",
+  title: "This is a generaated title",
+  description:
+    "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem corrupti debitis libero vitae saepe totam. Praesentium dolorem amet nihil placeat.",
+};
 
 export const myTemplateData = {
-    // Fill with your values
-    fileName:"mytemplate.html",
-    websiteTitle: "hello ",
-    title: "this is my template",
-    description: "this is my template description"
-}
+  // Fill with your values
+  fileName: "mytemplate.html",
+  websiteTitle: "Rizwan",
+  myName: "Md Rizwan Akram",
+  myDescription: "I am a Frontend engineeer at Verloop",
+  linkedIn: "https://www.linkedin.com/in/rizzwaan-akram/",
+  twitter: "https://twitter.com/RizwanAkram07",
+  github: "https://github.com/barbosa-07",
+};
--- index.mjs
+++ index.mjs
@@ -1,34 +1,31 @@
-import {readFile, writeFile} from 'fs/promises';
-import {sampleTemplateData, myTemplateData} from './config.mjs'
+import { readFile, writeFile } from "fs/promises";
+import { sampleTemplateData, myTemplateData } from "./config.mjs";
 
 //  reading file using URL
-let template = await readFile(new URL('template.html', import.meta.url), 'utf-8')
+let template = await readFile(
+  new URL("template.html", import.meta.url),
+  "utf-8"
+);
 
 //check for my template data
 
 const checkMyTemplate = (myTemplateData) => {
-    for (let [k,v] of Object.entries(myTemplateData)){
-        if(v=== ""){
-            return false
-        }else {
-            return true
-        }
+  for (let [k, v] of Object.entries(myTemplateData)) {
+    if (v === "") {
+      return false;
+    } else {
+      return true;
     }
-}
-
-
-
+  }
+};
 
 // Change this with your data
-const data = checkMyTemplate(myTemplateData) ? myTemplateData : sampleTemplateData
+const data = checkMyTemplate(myTemplateData)
+  ? myTemplateData
+  : sampleTemplateData;
 
-
-
-for ( let [k, v] of Object.entries(data)){
-    template = template.replace(`{${k}}`, v)
+for (let [k, v] of Object.entries(data)) {
+  template = template.replace(`{${k}}`, v);
 }
 
-await writeFile(new URL(data.fileName, import.meta.url), template)
-
-
-
+await writeFile(new URL(data.fileName, import.meta.url), template);
